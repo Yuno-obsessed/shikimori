8bcb31c36fa3191f49147347aad1b28f08acb107
Sekuoja <46537112+Sekuoja@users.noreply.github.com>
2022-10-08 01:07:42 +0300

    add function nice_advice

---
+++ internal/functions/advice.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func nice_advice() string {
+	resp, err := http.Get("http://fucking-great-advice.ru/api/random")
+	if err != nil {
+		fmt.Println("No response")
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	type response struct {
+		Id    int    `json:"id"`
+		Text  string `json:"text"`
+		Sound string `json:"sound"`
+	}
+	var data response
+	json.Unmarshal(body, &data)
+	return data.Text
+
+}
